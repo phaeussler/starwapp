451fadf03a5571d13d3749b742bac8e82ec07f2d
Pablo Hae <pahaeussler@uc.cl>
2019-03-25 00:29:11 +0000

    planet and people check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :execute_request, :get_name, :get_url
+  helper_method :execute_request, :get_name, :get_id
   BASE_URL = 'https://swapi.co/api'
 
   def execute_request(uri)
@@ -9,26 +9,32 @@ class ApplicationController < ActionController::Base
     response = open("#{BASE_URL}/#{uri}").read
     JSON.parse(response)
   end
+  
+  def get_all(type)
+    response = execute_request(type)
+    response['results']
+  end
 
-  def get_name(url)
+  def get_id(url, type='')
+    if url.include? BASE_URL
+      url.slice! BASE_URL+'/'+type
+      url[1...-1]
+    else
+      "Error get id"
+    end
+  end
+
+  def get_name(url, name="name")
     require 'open-uri'
     require 'json'
     if url.include? BASE_URL
       response = open(url).read
-      JSON.parse(response)['name']
+      JSON.parse(response)[name]
     else
       "Error"
     end
   end
 
-  def get_url(url)
-    if url.include? BASE_URL
-      url.slice! BASE_URL+'/'
-      url
-    else
-      "Error"
-    end
-  end
 
   def get_type(url)
     types = ['films', 'people', 'planets', 'starships']
